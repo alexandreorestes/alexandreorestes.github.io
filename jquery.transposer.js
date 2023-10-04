a9cf118b63a47f127dfba01889e1facbbebf6f0c
Alexandre Orestes Alves <alexandre.ufu@gmail.com>
2023-10-04 11:23:33 -0300

    Update jquery.transposer.js
    .

--- jquery.transposer.js
+++ jquery.transposer.js
@@ -1,10 +1,7 @@
 /*!
  * jQuery Chord Transposer plugin v1.0
- * http://codegavin.com/projects/transposer
  *
- * Copyright 2010, Jesse Gavin
- * Dual licensed under the MIT or GPL Version 2 licenses.
- * http://codegavin.com/license
+ * Copyright 2023, Alexandre Orestes
  *
  * Date: Sat Jun 26 21:27:00 2010 -0600
  */
